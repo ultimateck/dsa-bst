9da2bcbb487ed13b413c730ac78bc6ff2d704bf3
sandaruwanc <sandaruwanchanaka@gmail.com>
2014-02-18 22:50:29 +0530

    Exceptions

---
+++ src/dsa/bst/exceptions/FullTreeException.java
@@ -0,0 +1,11 @@
+package dsa.bst.exceptions;
+
+public class FullTreeException extends Exception{
+
+	public String messageForUser(){
+		
+		return "Full Tree Exception Happend....";
+		
+	}
+	
+}
---
+++ src/dsa/bst/exceptions/NotFoundException.java
@@ -0,0 +1,11 @@
+package dsa.bst.exceptions;
+
+public class NotFoundException extends Exception {
+
+	public String messageForUser(){
+		
+		return "Not Found Exception Happend....";
+		
+	}
+	
+}
